ac28583ed5e6092c3ef18a788bf42eb44af58b6e
Shuo Li <shuoli0503@gmail.com>
2025-07-15 17:23:23 -0700

    connect to TMDB api
    connect to api, load popular movies, shows movie poster and detail

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,34 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 import "../css/Home.css";
+import { getPopularMovies, searchMovies } from "../services/api";
 
 function Home() {
   const [searchInput, setSearchInput] = useState("");
-  const movies = [
-    {
-      id: 1,
-      url: "https://lumiere-a.akamaihd.net/v1/images/au_poster_movies_disney_zootopia2_teaser_b4cb9449.png",
-      title: "ZooTopia",
-      releaseDate: 2020,
-    },
-    {
-      id: 2,
-      url: "https://lumiere-a.akamaihd.net/v1/images/au_poster_movies_disney_zootopia2_teaser_b4cb9449.png",
-      title: "The Matrix",
-      releaseDate: 2021,
-    },
-    {
-      id: 3,
-      url: "https://lumiere-a.akamaihd.net/v1/images/au_poster_movies_disney_zootopia2_teaser_b4cb9449.png",
-      title: "ZooTopia3",
-      releaseDate: 2022,
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    function handleSearch(e) {
-        e.preventDefault()
-        // movies.filter((movie) =>movie.title.toLowerCase.includes(searchInput) )
-        setSearchInput("")
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovie = await getPopularMovies();
+        setMovies(popularMovie);
+      } catch (error) {
+        console.log(error);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
+
+  function handleSearch(e) {
+    e.preventDefault();
+    // movies.filter((movie) =>movie.title.toLowerCase.includes(searchInput) )
+    setSearchInput("");
   }
 
   function handleChange(e) {
@@ -42,16 +41,25 @@ function Home() {
           type="text"
           onChange={handleChange}
           className="search-input"
-                  placeholder="Search for movies"
-                  value={searchInput}
+          placeholder="Search for movies"
+          value={searchInput}
         />
-        <button type="submit" className="search-button">Search</button>
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </form>
-      <div className="movie-grid">
-              {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id}></MovieCard>
-        ))}
-      </div>
+
+      {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading</div>
+      ) : (
+        <div className="movie-grid">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id}></MovieCard>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,16 @@
 const BASE_URL = "https://api.themoviedb.org/3";
 const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
+async function getPopularMovies() {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${apiKey}`)
+    const data = await response.json()
+    return data.results
+}
+
+async function searchMovies(query) {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`)
+    const data = await response.json()
+    return data.results
+}
+
+export {getPopularMovies, searchMovies}
